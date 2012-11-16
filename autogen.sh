b2a53a459cc4294dec049d8d7f1b92ebb704f983
Colin Walters <walters@verbum.org>
2012-11-16 13:28:15 -0500

    autogen.sh: Honor NOCONFIGURE=1
    See http://people.gnome.org/~walters/docs/build-api.txt

--- autogen.sh
+++ autogen.sh
@@ -4,5 +4,7 @@ aclocal || exit 1
 autoheader || exit 1
 autoconf || exit 1
 automake -a -c || exit 1
-./configure --enable-maintainer-mode --enable-examples-build \
+if test -z "$NOCONFIGURE"; then
+    exec ./configure --enable-maintainer-mode --enable-examples-build \
 	--enable-x11-examples-build --enable-debug-log $*
+fi
