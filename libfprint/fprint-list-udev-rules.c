c14ebd8b636574cbe74aa05db0d0c1f6da9e2410
Bastien Nocera <hadess@hadess.net>
2013-07-25 12:29:53 +0200

    Add MS keyboard to the suspend blacklist
    045e:00bb isn't a stand-alone fingerprint reader, but a keyboard
    with a reader integrated.
    
    https://bugs.freedesktop.org/show_bug.cgi?id=66659

--- libfprint/fprint-list-udev-rules.c
+++ libfprint/fprint-list-udev-rules.c
@@ -30,6 +30,8 @@ static const struct usb_id whitelist_id_table[] = {
 
 static const struct usb_id blacklist_id_table[] = {
     { .vendor = 0x0483, .product = 0x2016 },
+    /* https://bugs.freedesktop.org/show_bug.cgi?id=66659 */
+    { .vendor = 0x045e, .product = 0x00bb },
     { 0, 0, 0 },
 };
 
