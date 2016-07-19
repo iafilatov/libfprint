9570c36fd42bff6246de50ace398a457b9495b46
Bastien Nocera <hadess@hadess.net>
2016-07-19 11:18:31 +0200

    build: Fix running autogen.sh out-of-tree

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+pushd $srcdir
+
 libtoolize --copy --force || exit 1
 aclocal || exit 1
 autoheader || exit 1
 autoconf || exit 1
 automake -a -c || exit 1
+popd
+
 if test -z "$NOCONFIGURE"; then
-    exec ./configure --enable-maintainer-mode --enable-examples-build \
+    $srcdir/configure --enable-maintainer-mode --enable-examples-build \
 	--enable-x11-examples-build --enable-debug-log $*
 fi
