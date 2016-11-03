12f6dae8cd81f363e02849acea40c69a8764f487
Bastien Nocera <hadess@hadess.net>
2016-11-03 14:05:11 +0100

    build: Fix 9570c36 on Debian
    Debian's ash doesn't implement pushd/popd.

--- autogen.sh
+++ autogen.sh
@@ -3,14 +3,16 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-pushd $srcdir
+olddir="`pwd`"
+
+cd "$srcdir"
 
 libtoolize --copy --force || exit 1
 aclocal || exit 1
 autoheader || exit 1
 autoconf || exit 1
 automake -a -c || exit 1
-popd
+cd "$olddir"
 
 if test -z "$NOCONFIGURE"; then
     $srcdir/configure --enable-maintainer-mode --enable-examples-build \
