iafilatov/libfprint

commit a0bbbd7d32150ab6ed25f466cce05169fbbbaead

drivers: Print USB error when libusb_claim_interface() fails
https://bugs.freedesktop.org/show_bug.cgi?id=98594

libfprint/drivers/etes603.c
@@ -1465,8 +1465,7 @@ static int dev_open(struct fp_img_dev *idev, unsigned long driver_data)
 
 	ret = libusb_claim_interface(idev->udev, 0);
 	if (ret != LIBUSB_SUCCESS) {
-		fp_err("libusb_claim_interface failed on interface 0 "
-		       "(err=%d)", ret);
+		fp_err("libusb_claim_interface failed on interface 0: %s", libusb_error_name(r));
 		return ret;
 	}
 

libfprint/drivers/aes1610.c
@@ -811,7 +811,7 @@ static int dev_init(struct fp_img_dev *dev, unsigned long driver_data)
 
 	r = libusb_claim_interface(dev->udev, 0);
 	if (r < 0) {
-		fp_err("could not claim interface 0");
+		fp_err("could not claim interface 0: %s", libusb_error_name(r));
 		return r;
 	}
 

libfprint/drivers/aes1660.c
@@ -53,7 +53,7 @@ static int dev_init(struct fp_img_dev *dev, unsigned long driver_data)
 
 	r = libusb_claim_interface(dev->udev, 0);
 	if (r < 0) {
-		fp_err("could not claim interface 0");
+		fp_err("could not claim interface 0: %s", libusb_error_name(r));
 		return r;
 	}
 

libfprint/drivers/aes2501.c
@@ -850,7 +850,7 @@ static int dev_init(struct fp_img_dev *dev, unsigned long driver_data)
 
 	r = libusb_claim_interface(dev->udev, 0);
 	if (r < 0) {
-		fp_err("could not claim interface 0");
+		fp_err("could not claim interface 0: %s", libusb_error_name(r));
 		return r;
 	}
 

libfprint/drivers/aes2550.c
@@ -622,7 +622,7 @@ static int dev_init(struct fp_img_dev *dev, unsigned long driver_data)
 
 	r = libusb_claim_interface(dev->udev, 0);
 	if (r < 0) {
-		fp_err("could not claim interface 0");
+		fp_err("could not claim interface 0: %s", libusb_error_name(r));
 		return r;
 	}
 

libfprint/drivers/aes2660.c
@@ -53,7 +53,7 @@ static int dev_init(struct fp_img_dev *dev, unsigned long driver_data)
 
 	r = libusb_claim_interface(dev->udev, 0);
 	if (r < 0) {
-		fp_err("could not claim interface 0");
+		fp_err("could not claim interface 0: %s", libusb_error_name(r));
 		return r;
 	}
 

libfprint/drivers/aes3500.c
@@ -131,7 +131,7 @@ static int dev_init(struct fp_img_dev *dev, unsigned long driver_data)
 
 	r = libusb_claim_interface(dev->udev, 0);
 	if (r < 0)
-		fp_err("could not claim interface 0");
+		fp_err("could not claim interface 0: %s", libusb_error_name(r));
 
 	aesdev = dev->priv = g_malloc0(sizeof(struct aes3k_dev));
 

libfprint/drivers/aes4000.c
@@ -128,7 +128,7 @@ static int dev_init(struct fp_img_dev *dev, unsigned long driver_data)
 
 	r = libusb_claim_interface(dev->udev, 0);
 	if (r < 0)
-		fp_err("could not claim interface 0");
+		fp_err("could not claim interface 0: %s", libusb_error_name(r));
 
 	aesdev = dev->priv = g_malloc0(sizeof(struct aes3k_dev));
 

commit 5a7e6e07ffa34fd4131f95fbfe5ca23d7eedc592

etes603: fix compilation error caused by a0bbbd7d3

libfprint/drivers/etes603.c
@@ -1465,7 +1465,7 @@ static int dev_open(struct fp_img_dev *idev, unsigned long driver_data)
 
 	ret = libusb_claim_interface(idev->udev, 0);
 	if (ret != LIBUSB_SUCCESS) {
-		fp_err("libusb_claim_interface failed on interface 0: %s", libusb_error_name(r));
+		fp_err("libusb_claim_interface failed on interface 0: %s", libusb_error_name(ret));
 		return ret;
 	}
 